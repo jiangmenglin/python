fd2de41424dad28947a79650abb4d33d4ae786ed
jiangmenglin <jiangmenglin@aliyun.com>
2018-03-27 20:54:27 +0800

    a byte of python

---
+++ abyteofpy/if.py
@@ -0,0 +1,13 @@
+number = 23
+guess = int(input('Enter an integer:'))
+
+if guess == number:
+    #New block starts here
+    print('Congratulations, you guessed it.')
+    #New block end here
+elif guess < number:
+    print('No, it is a lttile higher than that.')
+else:
+    print('No, it is a little lower than that.')
+
+print('Done.')
